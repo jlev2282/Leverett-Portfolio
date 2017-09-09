26d8f325edf9cbd27d76ca82cc635ee7f35af0c3
John <jlev2282@gmail.com>
2017-09-09 14:39:01 -0400

    scripts change

--- gulpfile.js
+++ gulpfile.js
@@ -106,9 +106,9 @@ gulp.task('connect', function() {
   console.log("App running on port 2207");
 });
 
-gulp.task("heroku:production", function(){
-    console.log('hello'); // the task does not need to do anything.
-});
+// gulp.task("heroku:production", function(){
+//     console.log('hello'); // the task does not need to do anything.
+// });
 // var port = process.env.PORT || 8000
 // server.listen(port, function() {
 //     console.log("App is running on port " + port);
